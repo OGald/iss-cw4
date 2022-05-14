002c1889ce97c27471c965a9be07e62187f211e0
visceral <visceralsec@gmail.com>
2022-05-14 17:27:44 +0100

    This make file really does like working :)

--- iss-cw4/solomon-scripts/solomon-sec-config.sh
+++ iss-cw4/solomon-scripts/solomon-sec-config.sh
@@ -2,8 +2,8 @@
 # Loads SELINUX security modules (Might be used for seccomp and other stuff)
 # Make sure you have execstart=/usr/bin/dockerd --selinux-enabled 
 # This is done in the /usr/lib/systemd/system/docker.service file
-sudo make -f /usr/share/selinux/devel/Makefile ../webserver/configfiles/docker-SEweb.pp
-sudo make -f /usr/share/selinux/devel/Makefile ../dbserver/configfiles/docker-SEdb.pp
+sudo make -f /usr/share/selinux/devel/Makefile ../webserver/configfiles/solomon-SEweb.pp
+sudo make -f /usr/share/selinux/devel/Makefile ../dbserver/configfiles/solomon-SEdb.pp
 
-sudo semodule -i ../dbserver/configFiles/docker-solomon-db.pp
-sudo semodule -i ../webserver/configFiles/docker-solomon-web.pp
+sudo semodule -i ../dbserver/configFiles/solomon-solomon-db.pp
+sudo semodule -i ../webserver/configFiles/solomon-solomon-web.pp
