e5760a2358d32e522f2ea53eab8ae9922eed2b1c
visceral <visceralsec@gmail.com>
2022-05-14 16:53:15 +0100

    fixed solomon sec config

--- iss-cw4/solomon-scripts/solomon-sec-config.sh
+++ iss-cw4/solomon-scripts/solomon-sec-config.sh
@@ -2,8 +2,8 @@
 # Loads SELINUX security modules (Might be used for seccomp and other stuff)
 # Make sure you have execstart=/usr/bin/dockerd --selinux-enabled 
 # This is done in the /usr/lib/systemd/system/docker.service file
-sudo make -f /usr/share/selinux/devel/Makefile docker-db.pp
-sudo make -f /usr/share/selinux/devel/Makefile docker-web.pp
+sudo make -f /usr/share/selinux/devel/Makefile ../webserver/configfiles/docker-SEweb.pp
+sudo make -f /usr/share/selinux/devel/Makefile ../dbserver/configfiles/docker-SEdb.pp
 
 sudo semodule -i ../webserver/docker-solomon-db.pp
 sudo semodule -i ../webserver/docker-solomon-web.pp
