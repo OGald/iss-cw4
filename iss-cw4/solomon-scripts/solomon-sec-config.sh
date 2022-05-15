5b61cc3868a69ee4e63eb11ea977e123fefac69d
visceral <visceralsec@gmail.com>
2022-05-15 12:53:58 +0100

    Changing Selinux policy names to fit the modules

--- iss-cw4/solomon-scripts/solomon-sec-config.sh
+++ iss-cw4/solomon-scripts/solomon-sec-config.sh
@@ -2,8 +2,10 @@
 # Loads SELINUX security modules (Might be used for seccomp and other stuff)
 # Make sure you have execstart=/usr/bin/dockerd --selinux-enabled 
 # This is done in the /usr/lib/systemd/system/docker.service file
-sudo make -f /usr/share/selinux/devel/Makefile ../webserver/configfiles/solomon-SEweb.pp
-sudo make -f /usr/share/selinux/devel/Makefile ../dbserver/configfiles/solomon-SEdb.pp
 
-sudo semodule -i ../dbserver/configFiles/solomon-SEdb.pp
-sudo semodule -i ../webserver/configFiles/solomon-SEweb.pp
+# Run these files in the respective config files once on your machine for selinux
+#sudo make -f /usr/share/selinux/devel/Makefile docker-solomon-web.pp
+#sudo make -f /usr/share/selinux/devel/Makefile docker-solomon-db.pp
+
+sudo semodule -i ../dbserver/configFiles/docker-solomon-web.pp
+sudo semodule -i ../webserver/configFiles/docker-solomon-db.pp
