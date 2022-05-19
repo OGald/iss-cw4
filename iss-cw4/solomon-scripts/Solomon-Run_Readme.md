5348ef38d32b052fc155e485c12b084d3331c564
OGald <owengaldridge@gmail.com>
2022-05-19 14:51:47 +0100

    changed 'solomon' to g42

--- iss-cw4/solomon-scripts/Solomon-Run_Readme.md
+++
@@ -1,6 +0,0 @@
-Simply just runs the docker run commands
-
-
-docker run -d --net iss2022/solomon_n --ip 192.0.2.201 --hostname db.cyber2022.test -e MYSQL_ROOT_PASSWORD="CorrectHorseBatteryStaple" -e MYSQL_DATABASE="iss2022db" --security-opt label:type:docker-solomon-db_t --name iss2022_solomon-db_c iss2022/solomon_i
-
-docker run -d --net iss2022/solomon_n --ip 192.0.2.202 --hostname www.cyber2022.test --add-host db.cyber2022.test:192.0.2.201 -p 80:80 --security-opt label:type:docker-solomon-web_t --name iss2022_solomon-web_c iss2022/solomon-web_i
--- iss-cw4/solomon-scripts/g42-build.sh
+++ iss-cw4/solomon-scripts/g42-build.sh
@@ -3,16 +3,16 @@
 # Run this script in the script folder
 
 # Creates docker webserver so the two containers can communicate
-docker network create --subnet=192.0.2.0/24 iss2022/solomon_n
+docker network create --subnet=192.0.2.0/24 iss2022/g42_n
 
 # Builds the database image
 cd ../dbserver/
-docker build -f Dockerfile --tag iss2022/solomon-db_i .
+docker build -f Dockerfile --tag iss2022/g42-db_i .
 
 # Builds the web image
 cd ..
 cd ../webserver/
-docker build -f Dockerfile --tag iss2022/solomon-web_i .
+docker build -f Dockerfile --tag iss2022/g42-web_i .
 
 #
 # _i = Image
