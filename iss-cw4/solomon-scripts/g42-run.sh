73ab5a3384efe1ad36c5874f38083733f479ba66
csc <csc@localhost.localdomain>
2022-05-20 22:00:02 +0100

    update

--- iss-cw4/solomon-scripts/g42-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Run this script to load the script
-
-# Runs the docker database image
-docker run -d --net iss2022/g42_n --ip 192.0.2.201 --hostname db.cyber2022.test -e MYSQL_ROOT_PASSWORD="CorrectHorseBatteryStaple" -e MYSQL_DATABASE="iss2022db" --security-opt label:type:docker-g42-db_t --name iss2022_g42-db_c iss2022/g42-db_i
-# Runs the docker 
-docker run -d --net iss2022/g42_n --ip 192.0.2.202 --hostname www.cyber2022.test --add-host db.cyber2022.test:192.0.2.201 -p 80:80 --security-opt label:type:docker-g42-web_t --name iss2022_g42-web_c iss2022/g42-web_i
