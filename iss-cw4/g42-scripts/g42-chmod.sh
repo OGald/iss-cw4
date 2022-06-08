44375f4618f056150ff857f61b674c62fa9f1b8a
Visceral-Sec <76218913+Visceral-Sec@users.noreply.github.com>
2022-06-08 23:09:32 +0100

    Delete iss-cw4 directory

--- iss-cw4/g42-scripts/g42-chmod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Sudo run this script just to chmod all of the other scripts so they can work
-chmod +x g42-build.sh
-chmod +x g42-remove.sh
-chmod +x g42-reset.sh
-chmod +x g42-restart.sh
-chmod +x g42-run.sh
-chmod +x g42-sec-config.sh
--- iss-cw4/webserver/configfiles/web_selinux_restart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo make -f /usr/share/selinux/devel/Makefile docker-g42-web.pp
-sudo semodule -i docker-g42-web.pp
